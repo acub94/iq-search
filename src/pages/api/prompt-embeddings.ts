125bcc44a54bc91fd58ce07307266c53e9741d86
Yadheedhya06 <Yadheedhya6@gmail.com>
2023-04-05 23:38:02 +0530

    similarity threshold update

--- src/pages/api/prompt-embeddings.ts
+++ src/pages/api/prompt-embeddings.ts
@@ -33,7 +33,7 @@ const handler = async (req: Request): Promise<Response> => {
     const { data: chunks, error } = await supabaseAdmin.rpc("langchain_search", {
       query_embedding: embedding,
       similarity_threshold: 0.76,
-      match_count: 4,
+      match_count: 5,
     });
     if (error) {
       console.error(error);
