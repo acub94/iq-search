4662d8cf55220c1eb32e48e396364380009344bf
Adebesin Cell <adebesintolulope80@gmail.com>
2023-04-11 16:31:40 +0100

    fixes css improvements

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -7,24 +7,24 @@ import {
   HStack,
   Flex,
   Tooltip,
-  useDisclosure,
-} from "@chakra-ui/react";
-import React, { useState } from "react";
-import Like from "./Data/Like.json";
-import Dislike from "./Data/DisLike.json";
-import Share from "./Data/share.json";
-import Lottie from "react-lottie";
-import ShareModal from "./ShareModal";
-import { shortenText } from "@/utils/shortenText";
-import { Inconsolata } from "next/font/google";
+  useDisclosure
+} from '@chakra-ui/react';
+import React, { useState } from 'react';
+import Like from './Data/Like.json';
+import Dislike from './Data/DisLike.json';
+import Share from './Data/share.json';
+import Lottie from 'react-lottie';
+import ShareModal from './ShareModal';
+import { shortenText } from '@/utils/shortenText';
+import { Inconsolata } from 'next/font/google';
 
 const inconsolata = Inconsolata({
-  subsets: ["latin"],
+  subsets: ['latin']
 });
 
 const ResultCard = ({
   result,
-  resultLink,
+  resultLink
 }: {
   result: string;
   resultLink: string;
@@ -34,8 +34,8 @@ const ResultCard = ({
     autoplay: false,
     animationData: Dislike,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
+      preserveAspectRatio: 'xMidYMid slice'
+    }
   };
 
   const defaultOptionsLike = {
@@ -43,8 +43,8 @@ const ResultCard = ({
     autoplay: false,
     animationData: Like,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
+      preserveAspectRatio: 'xMidYMid slice'
+    }
   };
 
   const defaultOptionsShare = {
@@ -52,8 +52,8 @@ const ResultCard = ({
     autoplay: false,
     animationData: Share,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
+      preserveAspectRatio: 'xMidYMid slice'
+    }
   };
   const [isStopped, setIsStopped] = useState(true);
   const [isPaused] = useState(false);
@@ -94,50 +94,53 @@ const ResultCard = ({
 
   return (
     <VStack
-      px="6"
-      pt="5"
-      pb="1"
-      bg="gray.50"
-      _dark={{ bg: "gray.700" }}
-      gap="1"
-      w={{ base: "80%", md: "80%", lg: "60%" }}
-      rounded="2xl"
+      px={{ base: 3, md: '6' }}
+      pt='5'
+      pb='1'
+      bg='gray.50'
+      _dark={{
+        bg: 'gray.700',
+        borderColor: '#ffffff3d',
+        borderWidth: '1px'
+      }}
+      gap='1'
+      w={{ base: '100%', md: '80%', xl: '60%' }}
+      rounded='2xl'
     >
-      <Box w="full">
-        <Text fontSize={{ base: "14px", lg: "16px" }} whiteSpace="pre-wrap">
+      <Box w='full'>
+        <Text fontSize={{ base: '14px', lg: '16px' }} whiteSpace='pre-wrap'>
           {result}
         </Text>
         <VStack
-          pt="7"
-          w="full"
-          alignItems="flex-start"
+          pt='7'
+          w='full'
+          alignItems='flex-start'
           fontFamily={inconsolata.style.fontFamily}
         >
-          <Flex w="full" pb="0">
-            Source:
+          <Flex w='full' pb='0'>
+            Source: <Text pl='2'>{shortenText(result, 70)}</Text>
+          </Flex>
+          <chakra.span>
             <Link
               href={resultLink}
-              color="brand.500"
-              _dark={{ color: "brand.800" }}
-              target="_blank"
-              textOverflow="hidden"
-              pl="2"
+              target='_blank'
+              textOverflow='hidden'
+              color='brand.500'
+              _hover={{ textDecor: 'underline' }}
+              _dark={{ color: 'brand.800' }}
             >
-              <Text>{shortenText(result, 70)}</Text>
-            </Link>{" "}
-          </Flex>
-          <chakra.span color="gray.500" _dark={{ color: "whiteAlpha.600" }}>
-            {resultLink}
+              {resultLink}
+            </Link>
           </chakra.span>
         </VStack>
       </Box>
       <HStack
-        w="full"
-        alignItems="center"
-        textAlign="center"
-        justifyContent="start"
+        w='full'
+        alignItems='center'
+        textAlign='center'
+        justifyContent='start'
       >
-        <Tooltip label="Share Link" fontSize="sm" rounded="md">
+        <Tooltip label='Share Link' fontSize='sm' rounded='md'>
           <chakra.div onClick={ShareHandler}>
             <Lottie
               options={defaultOptionsShare}
@@ -150,12 +153,11 @@ const ResultCard = ({
             />
           </chakra.div>
         </Tooltip>
-
-        <chakra.div pos="relative" cursor="pointer">
+        <chakra.div pos='relative' cursor='pointer'>
           <Lottie
             options={defaultOptionsLike}
             style={{
-              marginLeft: "-13px",
+              marginLeft: '-13px'
             }}
             height={50}
             width={50}
@@ -164,24 +166,23 @@ const ResultCard = ({
             speed={speedLike}
             direction={directionLike}
           />
-          <Tooltip label="Like" fontSize="sm" rounded="md">
+          <Tooltip label='Like' fontSize='sm' rounded='md'>
             <chakra.div
               onClick={LikeHandler}
-              pos="absolute"
-              top="30%"
-              left="3%"
-              w="18px"
-              h="20px"
+              pos='absolute'
+              top='30%'
+              left='3%'
+              w='18px'
+              h='20px'
             />
           </Tooltip>
         </chakra.div>
-
-        <chakra.div pos="relative" cursor="pointer">
+        <chakra.div pos='relative' cursor='pointer'>
           <Lottie
             options={defaultOptions}
             style={{
-              transform: "rotate(180deg)",
-              marginLeft: "-24px",
+              transform: 'rotate(180deg)',
+              marginLeft: '-24px'
             }}
             height={50}
             width={50}
@@ -190,14 +191,14 @@ const ResultCard = ({
             speed={speed}
             direction={direction}
           />
-          <Tooltip label="Dislike" fontSize="sm" rounded="md">
+          <Tooltip label='Dislike' fontSize='sm' rounded='md'>
             <chakra.div
               onClick={clickHandler}
-              pos="absolute"
-              top="30%"
-              left="-25%"
-              w="18px"
-              h="20px"
+              pos='absolute'
+              top='30%'
+              left='-25%'
+              w='18px'
+              h='20px'
             />
           </Tooltip>
         </chakra.div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Footer from "@/components/Footer";
-import { PGChunk } from "@/types";
-import Lottie from "lottie-react";
+import Footer from '@/components/Footer';
+import { PGChunk } from '@/types';
+import Lottie from 'lottie-react';
 import {
   Box,
   Flex,
@@ -12,44 +12,44 @@ import {
   chakra,
   useToast,
   Image,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import { Search2Icon } from "@chakra-ui/icons";
-import { ColorModeToggle } from "@/components/ColorToggle";
-import ResultCard from "@/components/ResultCard";
-import { useState } from "react";
-import endent from "endent";
-import FilterDark from "../components/Data/filterDark.json";
-import FilterLight from "../components/Data/filterLight.json";
-import Link from "next/link";
-import { queryReadyText } from "@/utils/shortenText";
+  useColorModeValue
+} from '@chakra-ui/react';
+import { Search2Icon } from '@chakra-ui/icons';
+import { ColorModeToggle } from '@/components/ColorToggle';
+import ResultCard from '@/components/ResultCard';
+import { useState } from 'react';
+import endent from 'endent';
+import FilterDark from '../components/Data/filterDark.json';
+import FilterLight from '../components/Data/filterLight.json';
+import Link from 'next/link';
+import { queryReadyText } from '@/utils/shortenText';
 
 export default function Home() {
-  const [queryText, setQueryText] = useState<string>("");
+  const [queryText, setQueryText] = useState<string>('');
   const [, setChunks] = useState<PGChunk[]>([]);
-  const [answer, setAnswer] = useState<string>("");
+  const [answer, setAnswer] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
-  const [resultId, setResultId] = useState("");
+  const [resultId, setResultId] = useState('');
   const toast = useToast();
 
   const handleAnswer = async () => {
     if (queryText.length === 0) {
       toast({
-        title: "Please enter a valid text before searching",
+        title: 'Please enter a valid text before searching',
         isClosable: true,
-        status: "error",
+        status: 'error'
       });
       return;
     }
     setLoading(true);
-    setAnswer("");
+    setAnswer('');
     const query = queryReadyText(queryText);
-    const searchResponse = await fetch("/api/prompt-embeddings", {
-      method: "POST",
+    const searchResponse = await fetch('/api/prompt-embeddings', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ query }),
+      body: JSON.stringify({ query })
     });
 
     if (!searchResponse.ok) {
@@ -63,7 +63,7 @@ export default function Home() {
       toast({
         title: "Search query can't be found in any wiki",
         isClosable: true,
-        status: "warning",
+        status: 'warning'
       });
       return;
     }
@@ -71,10 +71,10 @@ export default function Home() {
     setResultId(results[0].wikiid);
 
     let input = query;
-    if (input[input.length - 1] !== "?") {
-      input += "?";
+    if (input[input.length - 1] !== '?') {
+      input += '?';
     }
-    input = input.replace(/(\w)\?/g, "$1 ?");
+    input = input.replace(/(\w)\?/g, '$1 ?');
 
     const prompt = endent`
     Use the following passage to answer the query(dont write any questions in output): ${input}\n
@@ -83,15 +83,15 @@ export default function Home() {
       .map((chunk) => {
         return chunk.content;
       })
-      .join("")}
+      .join('')}
     `;
 
-    const answerResponse = await fetch("/api/answer", {
-      method: "POST",
+    const answerResponse = await fetch('/api/answer', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ prompt }),
+      body: JSON.stringify({ prompt })
     });
 
     if (!answerResponse.ok) {
@@ -120,107 +120,103 @@ export default function Home() {
   };
 
   const handleKeyPress = async (
-    event: React.KeyboardEvent<HTMLInputElement>,
+    event: React.KeyboardEvent<HTMLInputElement>
   ) => {
-    if (event.key === "Enter" && queryText.length > 0) {
+    if (event.key === 'Enter' && queryText.length > 0) {
       await handleAnswer();
-    } else if (event.key === "Enter" && queryText.length === 0) {
+    } else if (event.key === 'Enter' && queryText.length === 0) {
       toast({
-        title: "Please enter a valid text before searching",
+        title: 'Please enter a valid text before searching',
         isClosable: true,
-        status: "error",
+        status: 'error'
       });
     }
   };
 
   const style = {
     height: 100,
-    cursor: "pointer",
+    cursor: 'pointer'
   };
 
   const loadingSrc = useColorModeValue(FilterDark, FilterLight);
 
   return (
-    <Flex direction="column">
-      <Box w="full" textAlign="right" p="3" position="fixed">
+    <Flex direction='column'>
+      <Box w='full' textAlign='right' p='3' position='fixed'>
         <ColorModeToggle />
       </Box>
-      <chakra.div
-        minH="87vh"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
+      <chakra.div minH='87vh' display='flex' mt={{ md: '6' }}>
         <VStack
-          gap={{ base: "10", md: "6" }}
-          w="full"
-          mt={{ base: "16", lg: "12" }}
+          gap={{ base: '10', md: '6' }}
+          w='full'
+          mt={{ base: '16', lg: '12' }}
         >
-          <Link href="/">
+          <Link href='/'>
             <Image
-              src="./brainLogo.svg"
-              w={{ base: "180px", lg: "170px", xl: "160px" }}
+              src='./brainLogo.svg'
+              w={{ base: '120px' }}
+              alt='Braindao GPT logo'
             />
             <Heading
-              fontSize={{ lg: "35px", md: "3xl", base: "3xl" }}
-              pt="4"
-              textAlign="center"
-              _hover={{ textDecoration: "none" }}
+              fontSize={{ lg: '35px', md: '3xl', base: '3xl' }}
+              pt='4'
+              textAlign='center'
+              _hover={{ textDecoration: 'none' }}
             >
               IQ GPT
             </Heading>
           </Link>
-          <VStack w="full">
+          <VStack w='full' px={{ base: '5', md: 0 }}>
             <Flex
-              w={{ base: "80%", lg: "70%" }}
-              gap="2"
-              h="14"
-              borderColor="gray.200"
+              w={{ base: 'full', md: '560px' }}
+              gap='2'
+              h='14'
+              borderColor='gray.200'
               _dark={{
-                borderColor: "#ffffff3d",
-                bg: "gray.700",
-                color: "#ffffffa3",
+                borderColor: '#ffffff3d',
+                bg: 'gray.700',
+                color: '#ffffffa3'
               }}
-              bg="white"
-              borderWidth="1px"
-              rounded="lg"
-              pl="2"
-              alignItems="center"
+              bg='white'
+              borderWidth='1px'
+              rounded='lg'
+              pl='4'
+              alignItems='center'
             >
               <Input
-                placeholder="Ask me anything Crypto"
+                placeholder='Ask me anything Crypto'
                 _placeholderShown={{
-                  textOverflow: "ellipsis",
+                  textOverflow: 'ellipsis'
                 }}
-                fontSize="16"
+                fontSize='16'
                 value={queryText}
                 onChange={(e) => setQueryText(e.target.value)}
-                variant="unstyled"
+                variant='unstyled'
                 onKeyDown={handleKeyPress}
-                h="full"
+                h='full'
               />
               <Button
                 onClick={handleAnswer}
-                bg="none"
-                px="3"
-                _hover={{ bg: "none", color: "gray.500" }}
+                bg='none'
+                px='4'
+                _hover={{ bg: 'none', color: 'gray.500' }}
               >
                 <Icon as={Search2Icon} />
               </Button>
             </Flex>
 
             {loading ? (
-              <VStack py={{ base: "5", lg: "14" }}>
+              <VStack py={{ base: '5', lg: '14' }}>
                 <Lottie animationData={loadingSrc} style={style} />
               </VStack>
             ) : (
               <VStack
-                w="full"
-                justifyContent="center"
-                alignItems="center"
-                pt="4"
-                pb="6"
-                gap="3"
+                w='full'
+                justifyContent='center'
+                alignItems='center'
+                pt='4'
+                pb='6'
+                gap='3'
               >
                 {answer.length > 0 && (
                   <ResultCard
