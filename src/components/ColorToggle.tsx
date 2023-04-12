087b2fece7eacca653af851dee2c6d9856625aaf
Adebesin Cell <adebesintolulope80@gmail.com>
2023-04-12 15:09:30 +0100

    fixes ui suggestions and glitches

--- src/components/ColorToggle.tsx
+++ src/components/ColorToggle.tsx
@@ -1,20 +1,25 @@
-import { IconButton, IconButtonProps, useColorMode } from "@chakra-ui/react";
-import React from "react";
-import { RiMoonFill, RiSunFill } from "react-icons/ri";
+import { IconButton, IconButtonProps, useColorMode } from '@chakra-ui/react';
+import React from 'react';
+import { RiMoonFill, RiSunFill } from 'react-icons/ri';
 
-export const ColorModeToggle = (props: Omit<IconButtonProps, "aria-label">) => {
+export const ColorModeToggle = (props: Omit<IconButtonProps, 'aria-label'>) => {
   const { colorMode, toggleColorMode } = useColorMode();
 
-  const ColorModeIcon = colorMode === "light" ? RiMoonFill : RiSunFill;
+  const ColorModeIcon = colorMode === 'light' ? RiMoonFill : RiSunFill;
 
   return (
     <>
       <IconButton
-        aria-label="Color mode"
+        aria-label='Color mode'
         icon={<ColorModeIcon />}
         onClick={toggleColorMode}
-        variant="outline"
-        size="sm"
+        variant='ghost'
+        size='sm'
+        border='1px solid'
+        borderColor='gray.200'
+        _dark={{
+          borderColor: '#ffffff3d'
+        }}
         {...props}
       />
     </>
--- src/components/SearchCard/index.tsx
+++ src/components/SearchCard/index.tsx
@@ -1,26 +1,18 @@
-import {
-  VStack,
-  Text,
-  Link,
-  chakra,
-  Box,
-  Flex,
-  useDisclosure,
-} from "@chakra-ui/react";
-import React from "react";
-import { shortenText } from "@/utils/shortenText";
-import { Inconsolata } from "next/font/google";
-import CardActions from "./CardActions";
-import ShareModal from "../ShareModal";
+import { VStack, Text, Link, Box, useDisclosure, Flex } from '@chakra-ui/react';
+import React from 'react';
+import { shortenText } from '@/utils/shortenText';
+import { Inconsolata } from 'next/font/google';
+import CardActions from './CardActions';
+import ShareModal from '../ShareModal';
 
 const inconsolata = Inconsolata({
-  subsets: ["latin"],
+  subsets: ['latin']
 });
 
 const SearchCard = ({
   result,
   resultLink,
-  searchInput,
+  searchInput
 }: {
   searchInput: string;
   result: string;
@@ -33,20 +25,20 @@ const SearchCard = ({
 
   return (
     <VStack
-      p={{ base: 3, md: "5" }}
+      p={{ base: 3, md: '5' }}
       bg='gray.50'
       borderColor='gray.200'
       borderWidth='1px'
       _dark={{
-        bg: "gray.700",
-        borderColor: "#ffffff3d",
+        bg: 'gray.700',
+        borderColor: '#ffffff3d'
       }}
       gap='1'
-      w={{ base: "100%", md: "80%", xl: "60%" }}
+      w={{ base: '100%', md: '80%', xl: '60%' }}
       rounded='2xl'
     >
       <Box w='full'>
-        <Text fontSize={{ base: "14px", lg: "16px" }} whiteSpace='pre-wrap'>
+        <Text fontSize={{ base: '14px', lg: '16px' }} whiteSpace='pre-wrap'>
           {result}
         </Text>
         <VStack
@@ -55,21 +47,22 @@ const SearchCard = ({
           alignItems='flex-start'
           fontFamily={inconsolata.style.fontFamily}
         >
-          <Flex w='full' pb='0'>
-            Source: <Text pl='2'>{shortenText(result, 70)}</Text>
-          </Flex>
-          <chakra.span>
+          <Text mt='0'>
+            <Flex w='full' pb='0' as='span'>
+              Source: <Text pl='2'>{shortenText(result, 70)}</Text>
+            </Flex>
             <Link
+              mt='-5px'
               href={resultLink}
               target='_blank'
               textOverflow='hidden'
               color='brand.500'
-              _hover={{ textDecor: "underline" }}
-              _dark={{ color: "brand.800" }}
+              _hover={{ textDecor: 'underline' }}
+              _dark={{ color: 'brand.800' }}
             >
               {resultLink}
             </Link>
-          </chakra.span>
+          </Text>
         </VStack>
       </Box>
       <CardActions
