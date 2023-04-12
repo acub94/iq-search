e1dd91bc29382a4770519e727e4257efe3933695
Adebesin Cell <adebesintolulope80@gmail.com>
2023-04-12 15:52:32 +0100

    fixes footer border color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ import {
   Button,
   chakra,
   Link,
-  Icon,
-} from "@chakra-ui/react";
-import React from "react";
-import { Socials } from "./Data/socialData";
+  Icon
+} from '@chakra-ui/react';
+import React from 'react';
+import { Socials } from './Data/socialData';
 
 const Footer = () => {
   const thisYear = new Date().getFullYear();
@@ -23,19 +23,22 @@ const Footer = () => {
       justifyContent='center'
       fontWeight='semibold'
       color='#1A202C'
-      _dark={{ color: "white", borderTopColor: "#ffffff3d" }}
+      _dark={{
+        color: 'white',
+        borderTopColor: '#ffffff2d'
+      }}
       py='5'
       bottom='0'
     >
       <Text
         color='gray.600'
         _dark={{
-          color: "gray.400",
+          color: 'gray.400'
         }}
       >
-        Powered by{" "}
+        Powered by{' '}
         <Link
-          _hover={{ color: "whiteAlpha.900", textDecor: "underline" }}
+          _hover={{ color: 'whiteAlpha.900', textDecor: 'underline' }}
           href='https://braindao.org'
         >
           Braindao
@@ -46,11 +49,11 @@ const Footer = () => {
           <Link
             target='_blank'
             href={social.href}
-            _hover={{ textDecoration: "none" }}
+            _hover={{ textDecoration: 'none' }}
             key={social.href}
             color='gray.600'
             _dark={{
-              color: "gray.400",
+              color: 'gray.400'
             }}
           >
             <chakra.span srOnly>{social.href}</chakra.span>
@@ -58,13 +61,13 @@ const Footer = () => {
               <chakra.span srOnly>{social.href}</chakra.span>
               <Icon
                 as={social.icon}
-                w={{ base: "5", lg: "6" }}
-                h={{ base: "6", lg: "7" }}
+                w={{ base: '5', lg: '6' }}
+                h={{ base: '6', lg: '7' }}
                 color='gray.600'
                 _dark={{
-                  color: "gray.400",
+                  color: 'gray.400'
                 }}
-                _hover={{ color: "brand.500", _dark: { color: "brand.800" } }}
+                _hover={{ color: 'brand.500', _dark: { color: 'brand.800' } }}
               />
             </Button>
           </Link>
@@ -73,14 +76,14 @@ const Footer = () => {
 
       <Flex
         alignItems='center'
-        direction={{ base: "column", md: "initial" }}
-        fontSize={{ base: "12px", lg: "14px" }}
+        direction={{ base: 'column', md: 'initial' }}
+        fontSize={{ base: '12px', lg: '14px' }}
       >
         <Text
           pr={2}
           color='gray.600'
           _dark={{
-            color: "gray.400",
+            color: 'gray.400'
           }}
         >
           ©{thisYear} Powered by
@@ -89,7 +92,7 @@ const Footer = () => {
               as='span'
               color='#ff5caa'
               px='1'
-              _hover={{ textDecoration: "underline" }}
+              _hover={{ textDecoration: 'underline' }}
             >
               IQ.Wiki
             </Text>
