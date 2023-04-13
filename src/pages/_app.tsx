0d11e25ae5c1fcdd1f1f0874c15bed7fd6414d76
Srujan Gurram <srujangs8@gmail.com>
2023-04-13 20:16:41 +0530

    Remove React Query and QueryClientProvider from _app.tsx component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { Montserrat } from "next/font/google";
 import theme from "@/components/themes";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { trpc } from "@/utils/trpc";
 
 export const montserrat = Montserrat({
@@ -11,8 +10,6 @@ export const montserrat = Montserrat({
   weight: ["500", "600", "700"],
   display: "swap",
 });
-
-const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -22,11 +19,9 @@ const App = ({ Component, pageProps }: AppProps) => {
           --montserrat-font: ${montserrat.style.fontFamily};
         }
       `}</style>
-      <QueryClientProvider client={queryClient}>
-        <ChakraProvider theme={theme}>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </QueryClientProvider>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </StrictMode>
   );
 };
