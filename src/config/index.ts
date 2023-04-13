aa45c93c842eb559a2dfdb531228ab93213eef68
Srujan Gurram <srujangs8@gmail.com>
2023-04-13 21:36:38 +0530

    Refactor AvailableModels to use the zod.enum method and update the env variable reference in the graphqlUrl property of the config object

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,15 +1,25 @@
-import { AvailableModels } from "@/server/modules/getClubedResponse";
+import { env } from "@/env.mjs";
+import { z } from "zod";
+
+export const AvailableModels = z.enum([
+  "gpt-4",
+  "gpt-4-0314",
+  "gpt-4-32k",
+  "gpt-4-32k-0314",
+  "gpt-3.5-turbo",
+  "gpt-3.5-turbo-0301",
+]);
 
 const config = {
   graphqlUrl:
-    process.env.NEXT_PUBLIC_EP_API || "https://api.dev.braindao.org/graphql",
+    env.NEXT_PUBLIC_EP_API_URL || "https://api.dev.braindao.org/graphql",
   systemMessage:
     "You are an IQ.Wiki representative who loves to help people! Given the following sections from the IQ.Wiki articles, answer the questions using only that information in less than 150 words, outputted in the markdown format. If you are unsure and the answer is not explicitly written in the documentation, say -Sorry, I don't know how to help with that- .",
   defaultOptions: {
     pgFunction: "langchain150tkn_search",
     similarityThreshold: 0.76,
     matchCount: 5,
-    model: AvailableModels.Enum["gpt-3.5-turbo"],
+    model: AvailableModels.Values["gpt-3.5-turbo"],
     temperature: 0,
     maxTokens: 200,
   },
