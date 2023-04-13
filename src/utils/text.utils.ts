b6a0429b1226beb02b475f4b9a1a8430b6921d32
Srujan Gurram <srujangs8@gmail.com>
2023-04-13 20:17:17 +0530

    Add devLog function for logging chunks and transformed query in development environment. Update getBaseUrl function to remove unnecessary if statement

--- src/utils/text.utils.ts
+++ src/utils/text.utils.ts
@@ -1,3 +1,6 @@
+import { PGChunk } from "@/types";
+import endent from "endent";
+
 export const shortenText = (text: string, val?: number) => {
   const length = val ? val : 40;
   return text?.length > length ? `${text.substring(0, length)}...` : text;
@@ -9,4 +12,16 @@ export const transformQuery = (query: string): string => {
   input = input.replace(/(\w)\?/g, "$1 ?");
   input = input.charAt(0).toUpperCase() + input.slice(1);
   return input;
+};
+
+export const devLog = (transformedQuery: string, chunks: PGChunk[]) => {
+  if (process.env.NODE_ENV !== "development") return;
+  console.log("🛸 QUERY: ", transformedQuery);
+  chunks.forEach((chunk, i) =>
+    console.log(endent`
+    📄 CHUNK ${i + 1}
+    🛟 WIKI TITLE: ${chunk.title} 
+    🐳 CONTENT: ${chunk.content}
+    `),
+  );
 };
