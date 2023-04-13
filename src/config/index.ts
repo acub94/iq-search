c775897ef9457dac25f8e4cc6612e6ece029dc26
Srujan Gurram <srujangs8@gmail.com>
2023-04-13 19:30:40 +0530

    Add default options for language search using GPT-3 model in config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,17 @@
+import { AvailableModels } from "@/server/modules/getClubedResponse";
+
 const config = {
   graphqlUrl:
     process.env.NEXT_PUBLIC_EP_API || "https://api.dev.braindao.org/graphql",
+
+  defaultOptions: {
+    pgFunction: "langchain150tkn_search",
+    similarityThreshold: 0.76,
+    matchCount: 5,
+    model: AvailableModels.GPT_3_5_TURBO,
+    temperature: 0,
+    maxTokens: 200,
+  },
 };
 
 export default config;
