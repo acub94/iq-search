bd76f70d282fd0714baf6fa4d0fbde2cede1b84d
Srujan Gurram <srujangs8@gmail.com>
2023-04-13 19:30:28 +0530

    Transform queries to start with a capital letter in text.utils.ts and remove unused Supabase client in trpc.ts

--- src/utils/text.utils.ts
+++ src/utils/text.utils.ts
@@ -3,13 +3,10 @@ export const shortenText = (text: string, val?: number) => {
   return text?.length > length ? `${text.substring(0, length)}...` : text;
 };
 
-export const queryReadyText = (query: string): string => {
-  return query.replace(/(\w)\?/g, "$1 ?");
-};
-
 export const transformQuery = (query: string): string => {
   let input = query;
   if (input[input.length - 1] !== "?") input += "?";
   input = input.replace(/(\w)\?/g, "$1 ?");
+  input = input.charAt(0).toUpperCase() + input.slice(1);
   return input;
 };
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,22 +1,12 @@
 import { AppRouter } from "@/server/routes/_app";
 import { httpBatchLink } from "@trpc/client";
-import { createClient } from "@supabase/supabase-js";
 import { createTRPCNext } from "@trpc/next";
 
 function getBaseUrl() {
-  if (typeof window !== "undefined")
-    // browser should use relative path
-    return "";
+  if (typeof window !== "undefined") return "";
 
-  if (process.env.VERCEL_URL)
-    // reference for vercel.com
-    return `https://${process.env.VERCEL_URL}`;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
 
-  if (process.env.RENDER_INTERNAL_HOSTNAME)
-    // reference for render.com
-    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
-
-  // assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
@@ -25,29 +15,10 @@ export const trpc = createTRPCNext<AppRouter>({
     return {
       links: [
         httpBatchLink({
-          /**
-           * If you want to use SSR, you need to use the server's full URL
-           * @link https://trpc.io/docs/ssr
-           **/
           url: `${getBaseUrl()}/api/trpc`,
-
-          // You can pass any HTTP headers you wish here
-          async headers() {
-            return {
-              // authorization: getAuthCookie(),
-            };
-          },
         }),
       ],
     };
   },
-  /**
-   * @link https://trpc.io/docs/ssr
-   **/
   ssr: false,
 });
-
-export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!,
-);
