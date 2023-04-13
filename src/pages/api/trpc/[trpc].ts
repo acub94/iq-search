4ee39c5b9ac47fc2440ad7af8e3574bc74747024
Srujan Gurram <srujangs8@gmail.com>
2023-04-13 20:17:47 +0530

    fixed trpc returing 404

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,6 @@
 import { appRouter } from "@/server/routes/_app";
 import * as trpcNext from "@trpc/server/adapters/next";
 
-// export API handler
-// @see https://trpc.io/docs/api-handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => ({}),
