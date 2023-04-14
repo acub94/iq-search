db6ba819bc86662a78768e30760e9f3bd2698477
Yadheedhya06 <Yadheedhya6@gmail.com>
2023-04-14 15:47:00 +0530

    todo

---
+++ src/modules/refreshEmbeddings.ts
@@ -0,0 +1,10 @@
+// TODO: Fetch new wikis from IQ.WIKI
+// ------------- if first wiki is older than our cronjob frequency, then we can skip processing
+// ------------- continue with wikis you got in filtering process (new wikis) [wikis]
+// TODO: Clean the scrapped wikis
+
+// TODO: Chunk the content with langchain
+
+// TODO: Create embeddings of the chunks
+
+// TODO: Update supabase with the embeddings
