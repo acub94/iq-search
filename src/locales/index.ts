bd58a6d72d60f81b424c0edab74bf54d8bac172c
Srujan Gurram <srujangs8@gmail.com>
2023-04-18 17:36:29 +0530

    fixed korean routing

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,5 +2,5 @@ import { createI18n } from "next-international";
 
 export const { useI18n, I18nProvider, getLocaleProps } = createI18n({
   en: () => import("./en"),
-  fr: () => import("./kr"),
+  kr: () => import("./kr"),
 });
